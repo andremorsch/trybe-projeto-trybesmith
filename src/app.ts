69ac21696690e2dbf54264362cc2efbffacd5b19
andremorsch <andremorsch37@gmail.com>
2022-02-24 15:23:11 -0300

    req 3 - prepara caminho

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import UserController from './controller/User';
+import ProductController from './controller/Product';
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(express.json());
 app.post('/users', UserController.create);
 
 app.post('/login', UserController.login);
+
+app.post('/products', ProductController.create);
 
 export default app;
